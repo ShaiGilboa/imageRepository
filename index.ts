5e19365fab89c8ecb195baa0aec7fd94329e5d67
Shai Gilboa <shizel@gmail.com>
2020-09-01 19:27:22 -0400

    initial test of busboy

--- index.ts
+++ index.ts
@@ -1,4 +1,9 @@
 import express, { NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
+import Busboy from 'busboy';
+import path from 'path';
+import fs from 'fs';
+
 
 const PORT : number | string = process.env.PORT || 4000;
 
@@ -23,3 +28,41 @@ app.use((req : Request, res : Response, next : NextFunction) => {
   // the NextFunction is called to continue to the next step of any endpoint.
   next()
 })
+
+app.get('/', function (req, res) {
+  res.send('<html><head></head><body>\
+             <form method="POST" enctype="multipart/form-data">\
+              <input type="text" name="textfield"><br />\
+              <input type="file" name="filefield"><br />\
+              <input type="submit">\
+            </form>\
+          </body></html>');
+res.end();
+});
+
+app.post('/', (req: Request, res: Response) => {
+  console.log('req.headers', req.headers)
+  const busboy = new Busboy({headers: req.headers});
+  console.log('req.headers', req.headers)
+  busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+    file.on('data', (data) => {
+      console.log(`File [${fieldname}] got ${data.length} bytes`)
+    })
+    file.on('end', () => {
+      console.log(`File [${fieldname}] Finished`)
+    })
+    const saveTo = path.join(__dirname, 'dir', path.basename(filename))
+    console.log('saveTo', saveTo)
+    const outStream = fs.createWriteStream(saveTo);
+    file.pipe(outStream);
+  });
+  busboy.on('finish', () => {
+    console.log('test finish')
+    res.status(200).json({'connection': 'close'})
+    // res.end('end ')
+  })
+  console.log('return')
+  return req.pipe(busboy);
+})
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
