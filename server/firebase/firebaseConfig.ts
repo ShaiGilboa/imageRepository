c4be84b08408e87bdeaa3668052751d0e320b214
Shai Gilboa <shizel@gmail.com>
2020-09-04 16:40:58 -0400

    initial test of upload to storage

---
+++ server/firebase/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyBUbd97sUI1IWfkhI6YbiSbGxz8bPmtv10",
+  authDomain: "image-repository-a.firebaseapp.com",
+  databaseURL: "https://image-repository-a.firebaseio.com",
+  projectId: "image-repository-a",
+  storageBucket: "image-repository-a.appspot.com",
+  messagingSenderId: "523176563414",
+  appId: "1:523176563414:web:0679377c48d9ed36eb2f77"
+};
--- server/firebase/index.ts
+++ server/firebase/index.ts
@@ -1,2 +1,49 @@
 import * as admin from 'firebase-admin';
+import cert from './firebase-cerdentials.json';
+import firebaseConfig from './firebaseConfig';
+import { v4 as uuidv4 } from 'uuid';
+
+const serviceAccount = cert as admin.ServiceAccount
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: "image-repository-a.appspot.com",
+})
+
+const db = admin.firestore();
+const storage = admin.storage().bucket();
+// console.log('serviceAccount', serviceAccount)
+const docRef = db.collection('users').doc('alovelace');
+
+const upload = async (file : any) => {
+  const metadata = {
+    metadata: {
+      // This line is very important. It's to create a download token.
+      firebaseStorageDownloadTokens: uuidv4()
+    },
+    // contentType: 'image/png',
+    cacheControl: 'public, max-age=31536000',
+  };
+
+  // Uploads a local file to the bucket
+  await storage.upload(file, {
+    // Support for HTTP requests made with `Accept-Encoding: gzip`
+    gzip: true,
+    metadata: metadata,
+  });
+
+console.log(`${file} uploaded.`);
+
+}
+
+upload('C:/Users/User/Pictures/Camera Roll/1.jpg').catch(console.error);
+
+// const testing = async ()  => {
+//   await docRef.set({
+//     first: 'Ada',
+//     last: 'Lovelace',
+//     born: 1815
+//   });
+//   return;
+// }
 
